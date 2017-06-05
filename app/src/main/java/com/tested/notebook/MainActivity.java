2b941a5de8f645a42a4fb10d7889490d539448a3
pmmanishareddy <pmmanishareddy@gmail.com>
2017-06-05 19:45:32 +0530

    notebookjj

---
+++ app/src/main/java/com/tested/notebook/MainActivity.java
@@ -0,0 +1,32 @@
+package com.tested.notebook;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+        final TextView txtview = (TextView) findViewById(R.id.txtview);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, List.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        });
+    }
+}
